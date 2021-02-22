69c11a9b70a9f75117f699fa69f8edd5cc0c7360
hakidsdc <79456033+hakidsdc@users.noreply.github.com>
2021-02-22 14:34:44 +0100

    Create req19.sql

---
+++ req19.sql
@@ -0,0 +1,32 @@
+SELECT i_brand_id              brand_id, 
+               i_brand                 brand, 
+               i_manufact_id, 
+               i_manufact, 
+               Sum(ss_ext_sales_price) ext_price
+               
+FROM   store_sales, 
+customer,
+date_dim,
+customer_address,
+       item,
+       store
+WHERE  ss_item_sk = i_item_sk 
+AND d_date_sk = ss_sold_date_sk 
+AND i_manager_id = 38 
+AND d_moy = 12 
+AND d_year = 1998 
+AND ss_customer_sk=c_customer_sk
+AND c_current_addr_sk=ca_address_sk
+AND s_store_sk=ss_store_sk
+AND Substr(s_zip, 1, 5)<>Substr(ca_zip, 1, 5) 
+        
+GROUP  BY i_brand, 
+          i_brand_id, 
+          i_manufact_id, 
+          i_manufact 
+ORDER  BY ext_price DESC, 
+          i_brand, 
+          i_brand_id, 
+          i_manufact_id, 
+          i_manufact          
+LIMIT 100; 
